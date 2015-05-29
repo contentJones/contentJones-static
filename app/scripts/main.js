68df07a319663ef8b5863a9cd5c4b4c5064770fd
contentJones <pd@pauldavidson.info>
2015-05-29 13:08:48 -0600

    Complete redesign. contentJones 2.0.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+jQuery(document).ready(function() {
+	
+	'use strict';
+
+
+	//OFF VIEWPORT MOBILE MENU
+	jQuery('.toggle-nav').on('click', function() {
+	    // Calling a function in case you want to expand upon this.
+	    if (jQuery('#site-wrapper').hasClass('show-nav')) {
+	    // Do things on Nav Close
+	    jQuery('#site-wrapper').removeClass('show-nav');
+	    jQuery('.home #site-canvas').removeClass('screened');
+		} else {
+	    // Do things on Nav Open
+	    jQuery('#site-wrapper').addClass('show-nav');
+	    jQuery('.home #site-canvas').addClass('screened');
+	    }
+	    
+	});
+	
+});
